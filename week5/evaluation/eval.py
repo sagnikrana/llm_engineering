954b074de95b078fb44d4e1b4576f9657724328b
Sagnik Rana <sagnik.rana1994@gmail.com>
2026-03-01 14:23:03 -0500

    Updating the files

--- week5/evaluation/eval.py
+++ week5/evaluation/eval.py
@@ -10,7 +10,8 @@ from implementation.answer import answer_question, fetch_context
 
 load_dotenv(override=True)
 
-MODEL = "gpt-4.1-nano"
+# MODEL = "gpt-4.1-nano"
+MODEL = 'llama3.1:latest'
 db_name = "vector_db"
 
 
